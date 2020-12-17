69e9964f7ecce9607a9290117a81bc832f31e3b3
unknown <thom.florian.1997@googlemail.com>
2020-12-17 02:09:22 +0100

    Add scss and minor changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'BusinessModelDigitalHealthAlgorithm2'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual("BusinessModelDigitalHealthAlgorithm2");
-  });
-
   it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,3 @@
-// For easier debugging in development mode, you can import the following file
-import "zone.js/dist/zone-error";
-
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 export const environment = {
   production: false
