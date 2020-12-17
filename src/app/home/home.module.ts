1c8d2026ff7b8e325d49003450c0cc447adab01a
unknown <thom.florian.1997@googlemail.com>
2020-12-17 03:31:58 +0100

    Scaffolded things for spider-chart

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from "./home.component";
 
 import { HomeRoutingModule } from "./home-routing.module";
 import { Page404HomeComponentComponent } from './page404-home-component/page404-home-component.component';
+import { SpiderchartComponent } from './spiderchart/spiderchart.component';
 
 @NgModule({
-  declarations: [HomeComponent, Page404HomeComponentComponent],
+  declarations: [HomeComponent, Page404HomeComponentComponent, SpiderchartComponent],
   imports: [CommonModule, HomeRoutingModule]
 })
 export class HomeModule {}
---
+++ src/app/home/spiderchart/spiderchart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spiderchart',
+  templateUrl: './spiderchart.component.html',
+  styleUrls: ['./spiderchart.component.scss']
+})
+export class SpiderchartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
