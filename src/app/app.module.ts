fb6a16d45a64ec7436a93113e9095411d079a401
unknown <thom.florian.1997@googlemail.com>
2020-12-17 03:12:56 +0100

    Scaffolded min ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, StoreModule.forRoot(reducers, {
+      metaReducers
+    })],
   providers: [],
   bootstrap: [AppComponent]
 })
