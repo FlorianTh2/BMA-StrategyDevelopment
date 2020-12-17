52489d0f966d9d58094d7dce4acbc4787f5c014f
unknown <thom.florian.1997@googlemail.com>
2020-12-17 01:54:58 +0100

    Added eslint and prettier

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // For easier debugging in development mode, you can import the following file
-import 'zone.js/dist/zone-error';
+import "zone.js/dist/zone-error";
 
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-export const environment ={
+export const environment = {
   production: false
 };
