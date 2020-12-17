a1f89b17f95427ec5d26d0c51b1e5885a14ebfe7
unknown <thom.florian.1997@googlemail.com>
2020-12-17 14:56:07 +0100

    Minor changes

--- src/app/home/spiderchart/spiderchart.component.ts
+++ src/app/home/spiderchart/spiderchart.component.ts
@@ -1,13 +1,41 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewEncapsulation
+} from "@angular/core";
+import * as d3 from "d3";
 
 @Component({
   selector: "app-spiderchart",
   templateUrl: "./spiderchart.component.html",
-  styleUrls: ["./spiderchart.component.scss"]
+  styleUrls: ["./spiderchart.component.scss"],
+  encapsulation: ViewEncapsulation.None
 })
 export class SpiderchartComponent implements OnInit {
   title: string = "Spiderchart with d3.js";
-  constructor() {}
+  private width: number;
+  private height: number;
+  private container: any;
+  hostElement: any;
+  private data = [
+    { id: "d1", value: 10, region: "USA" },
+    { id: "d2", value: 11, region: "India" },
+    { id: "d3", value: 12, region: "China" },
+    { id: "d4", value: 6, region: "Germany" }
+  ];
+  constructor(private elRef: ElementRef) {
+    this.hostElement = this.elRef.nativeElement;
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.container = d3
+      .select(this.hostElement)
+      .select("svg")
+      .classed("container", true);
+
+    this.renderChart();
+  }
+
+  private renderChart() {}
 }
