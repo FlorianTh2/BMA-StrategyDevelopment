1efd149ebe1333c123309df9de81ed48b54e6247
unknown <thom.florian.1997@googlemail.com>
2020-12-18 12:21:26 +0100

    Added material-module to shared-module

--- src/app/shared/share.module.ts
+++ src/app/shared/share.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SafeHtmlPipe } from "./pipes/safe-html.pipe";
+import { MaterialModule } from "../material/material.module";
 
 @NgModule({
   declarations: [SafeHtmlPipe],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
   exports: [SafeHtmlPipe]
 })
 export class SharedModule {}
