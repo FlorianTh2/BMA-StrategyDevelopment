20e0baed11aa72660f3ba8b1e6016e46c7c9acea
unknown <thom.florian.1997@googlemail.com>
2020-12-21 18:49:17 +0100

    Minor changes

--- src/app/static-sites/static-sites.module.ts
+++ src/app/static-sites/static-sites.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { StaticSitesRoutingModule } from './static-sites-routing.module';
-import { AboutComponent } from './about/about.component';
-import { PrivacyComponent } from './privacy/privacy.component';
-import { ImpressumComponent } from './impressum/impressum.component';
-
+import { AboutComponent } from "./about/about.component";
+import { PrivacyComponent } from "./privacy/privacy.component";
+import { ImpressumComponent } from "./impressum/impressum.component";
 
 @NgModule({
   declarations: [AboutComponent, PrivacyComponent, ImpressumComponent],
-  imports: [
-    CommonModule,
-    StaticSitesRoutingModule
-  ]
+  imports: [CommonModule]
 })
-export class StaticSitesModule { }
+export class StaticSitesModule {}
