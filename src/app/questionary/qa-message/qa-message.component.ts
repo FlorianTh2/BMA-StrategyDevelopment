39160581ad51965bba633cd1152b0ffeb470c825
unknown <thom.florian.1997@googlemail.com>
2020-12-21 19:42:02 +0100

    Added input for each qa-message-component

--- src/app/questionary/qa-message/qa-message.component.ts
+++ src/app/questionary/qa-message/qa-message.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Message } from "../shared/models/message.model";
 
 @Component({
-  selector: 'app-qa-message',
-  templateUrl: './qa-message.component.html',
-  styleUrls: ['./qa-message.component.scss']
+  selector: "app-qa-message",
+  templateUrl: "./qa-message.component.html",
+  styleUrls: ["./qa-message.component.scss"]
 })
 export class QaMessageComponent implements OnInit {
+  @Input()
+  message: Message;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
