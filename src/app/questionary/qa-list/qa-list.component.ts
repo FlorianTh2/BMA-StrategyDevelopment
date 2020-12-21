126e166b2bad0b4f54d160540a91c43872e61278
unknown <thom.florian.1997@googlemail.com>
2020-12-21 19:20:09 +0100

    Created enum and some example-messages

--- src/app/questionary/qa-list/qa-list.component.ts
+++ src/app/questionary/qa-list/qa-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Message } from "../shared/models/message.model";
 
 @Component({
-  selector: 'app-qa-list',
-  templateUrl: './qa-list.component.html',
-  styleUrls: ['./qa-list.component.scss']
+  selector: "app-qa-list",
+  templateUrl: "./qa-list.component.html",
+  styleUrls: ["./qa-list.component.scss"]
 })
 export class QaListComponent implements OnInit {
+  MESSAGES: Message[] = [
+    { id: 0, sender: Sender.System, content: "Welcome" },
+    { id: 1, sender: Sender.User, content: "Welcome" },
+    { id: 2, sender: Sender.System, content: "Welcome" },
+    { id: 3, sender: Sender.User, content: "Welcome" },
+    { id: 4, sender: Sender.System, content: "Welcome" }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/questionary/shared/enums/senderEnum.ts
@@ -0,0 +1,4 @@
+enum Sender {
+  System,
+  User
+}
