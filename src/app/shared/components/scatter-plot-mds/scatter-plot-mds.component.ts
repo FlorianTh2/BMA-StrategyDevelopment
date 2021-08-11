a732a5152edf577fa38002e69cda9d326dd8df1b
unknown <thom.florian.1997@googlemail.com>
2021-08-11 21:38:49 +0200

    Minor adjustments on mds scatter plot

--- src/app/shared/components/scatter-plot-mds/scatter-plot-mds.component.ts
+++ src/app/shared/components/scatter-plot-mds/scatter-plot-mds.component.ts
@@ -158,16 +158,42 @@ export class ScatterPlotMdsComponent implements OnInit {
         // .tickPadding(10)
       );
 
+    // topic: proper placement of grid and axis labels
+    // procedure: get min and max value of xscale, get the percentage from 1. x<0 (negativ) to x=0 2. from 0 to x>0
+    // then transfer this given relationship to this.width (separate this.width into 2 parts: 1. from x<0 to 0 and 0 to x>0)
+    // why?
+    //  the grid will be printed FROM the y-axis (yes y-axis (aka x=0) -> at each tick at y-axis there will be printed a pseudo parallel
+    //  line to the x-axis so that we get this lines ---), but we cannot just take this.width/2 since we cannot assume that
+    //  x=0 (aka y-axis lies at the center of the svg (aka this.width/2))
+    const xMin = this.xscale.domain()[0];
+    const xMax = this.xscale.domain()[this.xscale.domain().length - 1];
+    // pixel from x<0 to x=0
+    const pixelToYAxis =
+      (Math.abs(xMin) / (Math.abs(xMin) + Math.abs(xMax))) * this.width;
+    // pixel from x=0 to x>0
+    const pixelFromYAxis =
+      (Math.abs(xMax) / (Math.abs(xMin) + Math.abs(xMax))) * this.width;
+
+    const yMin = this.yscale.domain()[0];
+    const yMax = this.yscale.domain()[this.yscale.domain().length - 1];
+    const pixelToXAxis =
+      (Math.abs(yMax) / (Math.abs(yMin) + Math.abs(yMax))) * this.height;
+    const pixelFromXAxis =
+      (Math.abs(yMin) / (Math.abs(yMin) + Math.abs(yMax))) * this.height;
+
+    // offset, to move labels away from coordinate origin
+    const labelAdjustment = 50;
+
     const axisBottomLabel = g1
       .selectAll(".gAxisBottomLabel")
       .data([-1])
       .enter()
       .append("text")
       .attr("class", "gAxisBottomLabel")
-      .attr("x", this.width / 2)
+      .attr("x", pixelToYAxis + labelAdjustment)
       // pay attention: /2 is only applied to this.margin.bottom ()
       // .attr("y", this.height + this.margin.top + (this.margin.bottom / 2))
-      .attr("y", (this.height + this.margin.top + this.margin.bottom / 2) / 2)
+      .attr("y", pixelToXAxis + labelAdjustment)
       .text("x");
 
     const axisLeft = g1
@@ -190,17 +216,14 @@ export class ScatterPlotMdsComponent implements OnInit {
       .enter()
       .append("text")
       .attr("class", "gAxisLeftLabel")
-      .attr("x", -this.height / 2)
-      .attr("y", -this.margin.left / 2)
+      .attr("x", -1 * pixelToXAxis + labelAdjustment)
+      // .attr("y", -this.margin.left / 2)
       // dx, dy is not affected of rotate, so to bring label to middle (instead of after rotating outer left)
-      .attr("dx", "50px")
-      .attr("dy", this.width / 2 + "px")
+      // .attr("dx", "50px")
+      .attr("dy", pixelToYAxis - labelAdjustment)
       .attr("transform", "rotate(-90)") // translateX(100px)
       .style("text-anchor", "end")
       .text("y");
-
-    const xMin = this.xscale.domain()[0];
-    const xMax = this.xscale.domain()[this.xscale.domain().length - 1];
 
     // https://stackoverflow.com/questions/40766379/d3-adding-grid-to-simple-line-chart
     // in grid these lines
@@ -213,25 +236,10 @@ export class ScatterPlotMdsComponent implements OnInit {
       .style("stroke", "black")
       .style("shape-rendering", "crispEdges")
       .style("stroke-opacity", 0.2)
-      .attr(
-        "x1",
-        -(Math.abs(xMin) / (Math.abs(xMin) + Math.abs(xMax))) * this.width
-      )
+      .attr("x1", -1 * pixelToYAxis)
       .attr("y1", 0)
-      .attr(
-        "x2",
-        // procedure: get min and max value of xscale, get the percentage from 1. x<0 (negativ) to x=0 2. from 0 to x>0
-        // then transfer this given relationship to this.width (separate this.width into 2 parts: 1. from x<0 to 0 and 0 to x>0)
-        // why?
-        //  the grid will be printed FROM the y-axis (yes y-axis (aka x=0) -> at each tick at y-axis there will be printed a pseudo parallel
-        //  line to the x-axis so that we get this lines ---), but we cannot just take this.width/2 since we cannot assume that
-        //  x=0 (aka y-axis lies at the center of the svg (aka this.width/2))
-        (Math.abs(xMax) / (Math.abs(xMin) + Math.abs(xMax))) * this.width
-      )
+      .attr("x2", pixelFromYAxis)
       .attr("y2", 0);
-
-    const yMin = this.yscale.domain()[0];
-    const yMax = this.yscale.domain()[this.yscale.domain().length - 1];
 
     // in grid these lines
     // ||||
@@ -244,15 +252,9 @@ export class ScatterPlotMdsComponent implements OnInit {
       .style("shape-rendering", "crispEdges")
       .style("stroke-opacity", 0.2)
       .attr("x1", 0)
-      .attr(
-        "y1",
-        (Math.abs(yMin) / (Math.abs(yMin) + Math.abs(yMax))) * this.height
-      )
+      .attr("y1", -1 * pixelToXAxis)
       .attr("x2", 0)
-      .attr(
-        "y2",
-        -(Math.abs(yMax) / (Math.abs(yMin) + Math.abs(yMax))) * this.height
-      );
+      .attr("y2", pixelFromXAxis);
 
     const scatterPointsOnPathLine = g1
       .selectAll(".gPathpoints")
